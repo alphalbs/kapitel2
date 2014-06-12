9a9ec1fbb7071e1a38ae3511976ce806d5902b7a
Philipp Heisig <github@pheisig.de>
2014-06-12 14:23:33 +0200

    verschiedene Muellsorten mit verschiedenen Objekten hinzugefuegt

--- kap2.js
+++ kap2.js
@@ -18,7 +18,38 @@ function preload() {
     game.load.image('biomuell', 'assets/biomuell_labeled.png');
     game.load.image('sondermuell', 'assets/sondermuell_labeled.png');
 
-    game.load.image('apfel', 'assets/apfel.png');
+    game.load.image('apfel', 'assets/muell/bio/apfel.png');
+    game.load.image('teebeutel', 'assets/muell/bio/teebeutel.png');
+
+    game.load.image('brot', 'assets/muell/bio/brot.png');
+    game.load.image('fischgraete', 'assets/muell/bio/fischgraete.png');
+    game.load.image('gemuese', 'assets/muell/bio/gemuese.png');
+    game.load.image('laub', 'assets/muell/bio/laub.png');
+    game.load.image('bananenschale', 'assets/muell/bio/bananenschale.png');
+
+    game.load.image('braunglas', 'assets/muell/glas/braunglas.png');
+    game.load.image('nutellaglas', 'assets/muell/glas/nutellaglas.png');
+    game.load.image('weinflasche', 'assets/muell/glas/weinflasche.png');
+
+    game.load.image('duschgel', 'assets/muell/gruener_punkt/duschgel.png');
+    game.load.image('joghurtbecher', 'assets/muell/gruener_punkt/joghurtbecher.png');
+    game.load.image('konserve', 'assets/muell/gruener_punkt/konserve.png');
+    game.load.image('plastikbeutel', 'assets/muell/gruener_punkt/plastikbeutel.png');
+    game.load.image('tetrapack', 'assets/muell/gruener_punkt/tetrapack.png');
+
+    game.load.image('briefumschlaege', 'assets/muell/papier/briefumschlaege.png');
+    game.load.image('karton', 'assets/muell/papier/karton.png');
+    game.load.image('zeitung', 'assets/muell/papier/zeitung.png');
+
+    game.load.image('asche', 'assets/muell/rest/asche.png');
+    game.load.image('hundehaufen', 'assets/muell/rest/hundehaufen.png');
+    game.load.image('kugelschreiber', 'assets/muell/rest/kugelschreiber.png');
+    game.load.image('spielzeug', 'assets/muell/rest/spielzeug.png');
+
+    game.load.image('batterie', 'assets/muell/sonder/batterie.png');
+    game.load.image('energiesparlampe', 'assets/muell/sonder/energiesparlampe.png');
+
+
 
     game.load.image('background', 'assets/background.jpg');
 }
@@ -39,6 +70,13 @@ function create() {
     trash_group = game.add.group();
     trash_group.enableBody = true;
 
+    rest = new Array("asche","hundehaufen","kugelschreiber","spielzeug");
+    papier = new Array("briefumschlaege","karton","zeitung");
+    gp = new Array("konserve","plastikbeutel","duschgel","joghurtbecher","tetrapack");
+    glas = new Array("weinflasche","nutellaglas","braunglas");
+    bio = new Array("apfel","teebeutel","laub","gemuese","bananenschale","brot","fischgraete");
+    sonder = new Array("batterie","energiesparlampe");
+
     createTrash();
 }
 
@@ -50,10 +88,45 @@ function createCounter() {
 }
 
 function createTrash() {
-    var random = game.rnd.integerInRange(200, 560);
-    trash = trash_group.create(random, 0, 'apfel');
-    trashtype = "bio";
-    trash.body.gravity.y = 100;
+    var random1 = game.rnd.integerInRange(0, 5);
+    var random2 = game.rnd.integerInRange(200, 560);
+    switch(random1) {
+        case 0:
+            var random3 = game.rnd.integerInRange(0, rest.length - 1);
+            trash = trash_group.create(random2, 0, rest[random3]);
+            trashtype = "rest";
+            break;
+        case 1:
+            var random3 = game.rnd.integerInRange(0, papier.length - 1);
+            trash = trash_group.create(random2, 0, papier[random3]);
+            trashtype = "papier";
+            break;
+        case 2:
+            var random3 = game.rnd.integerInRange(0, gp.length - 1);
+            trash = trash_group.create(random2, 0, gp[random3]);
+            trashtype = "gp";
+            break;
+        case 3:
+            var random3 = game.rnd.integerInRange(0, glas.length - 1);
+            trash = trash_group.create(random2, 0, glas[random3]);
+            trashtype = "glas";
+            break;
+        case 4:
+            var random3 = game.rnd.integerInRange(0, bio.length - 1);
+            trash = trash_group.create(random2, 0, bio[random3]);
+            trashtype = "bio";
+            break;
+        case 5:
+            var random3 = game.rnd.integerInRange(0, sonder.length - 1);
+            trash = trash_group.create(random2, 0, sonder[random3]);
+            trashtype = "sonder";
+            break;
+        default:
+            trash = trash_group.create(random2, 0, apfel);
+            trashtype = "bio";
+            break;
+    }
+    trash.body.gravity.y = 50;
 }
 
 function update() {
@@ -111,7 +184,7 @@ function createButtons() {
     papiermuell.inputEnabled = true;
     papiermuell.events.onInputDown.add(function() { block.loadTexture('papiermuell_tonne', 0); akt_tonne = "papier"; }, this);
     gelber_sack.inputEnabled = true;
-    gelber_sack.events.onInputDown.add(function() { block.loadTexture('gelber_sack_tonne', 0); akt_tonne = "gelb"; }, this);
+    gelber_sack.events.onInputDown.add(function() { block.loadTexture('gelber_sack_tonne', 0); akt_tonne = "gp"; }, this);
     glas.inputEnabled = true;
     glas.events.onInputDown.add(function() { block.loadTexture('glas_tonne', 0); akt_tonne = "glas"; }, this);
     biomuell.inputEnabled = true;
